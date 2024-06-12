2113b07c853b108a15ec436541ebb668333cddb3
LinkedBlades <pablo.99.garcia.sachetti@gmail.com>
2024-06-11 23:22:30 -0300

    Merged Prototypes into GameDevProject
    Just added the prototypes into one project I think we should use to develop the game in from now on. Please go through the files to see if you guys are fine with the structure of the project.

---
+++ GameDevGroupProject/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private BoxCollider2D bc;
+
+    [SerializeField]
+    float jumpSpeed;
+
+    [SerializeField]
+    float moveSpeed;
+
+    [SerializeField]
+    float fallAugmentMultiplier;
+
+    [SerializeField]
+    float fallAugmentThreshold;
+
+    private PlayerInput playerInput;
+    private InputAction playerMove;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Make a reference for the Move action from the player input
+        playerInput = GetComponent<PlayerInput>();
+        playerMove = playerInput.actions.FindAction("Move");
+
+        // Make a reference for the rigidbody attached to the player
+        rb = GetComponent<Rigidbody2D>();
+
+        // Freeze the rotation of the player
+        rb.freezeRotation = true;
+
+        bc = GetComponent<BoxCollider2D>();
+    }
+
+    void FixedUpdate(){
+        // Move the player left/right using input
+        rb.velocity = new Vector2(moveSpeed * playerMove.ReadValue<Vector2>().x, rb.velocity.y);
+
+        // Increase downwards velocity linearly after a certain threshold, creating acceleration
+        if (rb.velocity.y < fallAugmentThreshold){
+            float fallAugment = (fallAugmentThreshold - rb.velocity.y) * fallAugmentMultiplier;
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y - fallAugment);
+        }
+    }
+
+    void OnJump(){
+        if (isGrounded()){
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+    }
+
+    bool isGrounded(){
+        // Since the player is a square, two raycasts are needed to determine if they are on the ground
+        // One raycast from left, one from right
+        Vector2 playerLeft = new Vector2(transform.position.x - bc.bounds.extents.x, transform.position.y);
+        Vector2 playerRight = new Vector2(transform.position.x + bc.bounds.extents.x, transform.position.y);
+        RaycastHit2D groundCheckLeft = Physics2D.Raycast(playerLeft, Vector2.down, bc.bounds.extents.y + 0.1f);
+        RaycastHit2D groundCheckRight = Physics2D.Raycast(playerRight, Vector2.down, bc.bounds.extents.y + 0.1f);
+
+        return groundCheckLeft || groundCheckRight;
+    }
+}
---
+++ GameDevGroupProject/Assets/Scripts/robotDetection.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class robotDetection : MonoBehaviour
+{
+
+    [SerializeField]
+    float fov = 25f;
+    /*[SerializeField]
+    bool moving = false;*/
+    [SerializeField]
+    bool facingRight = true; // if false, then facing left
+    [SerializeField]
+    int numRaycasts = 2;
+    private Vector2 currentPos;
+
+    void OnEnable(){
+
+        Mesh mesh = new Mesh();
+        mesh.name = "robotFOV";
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        PolygonCollider2D pc = gameObject.AddComponent<PolygonCollider2D>();
+        pc.isTrigger = true;
+        Vector2[] points = new Vector2[numRaycasts + 1];
+
+        setRobotPos();
+
+        Vector3[] vertices = new Vector3[points.Length];
+        Vector2[] uvs = new Vector2[points.Length];
+        int[] triangles = new int[3 * (numRaycasts - 1)];
+
+        points[0] = Vector2.zero;
+        vertices[0] = Vector3.zero;
+
+        int xDir = 1;
+        if (!facingRight){
+            xDir = -1;
+            fov = -fov;
+        }
+
+        float increments = fov/(numRaycasts - 1);
+
+        int triIndex = 0;
+
+        for (int i = 1; i <= numRaycasts; i++){
+            float yDir = Mathf.Tan(((fov / 2f) - increments * (i - 1)) * Mathf.PI / 180f);
+            Vector2 directionVector = new Vector2(xDir, yDir);
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(currentPos, directionVector);
+            points[i] = raycastHit2D.point - currentPos;
+            vertices[i] = points[i];
+
+            if (i < numRaycasts){
+                triangles[triIndex] = 0;
+                triangles[triIndex + 1] = i;
+                triangles[triIndex + 2] = i + 1;
+                triIndex += 3;
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uvs;
+        mesh.triangles = triangles;
+
+        pc.SetPath(0, points);
+    }
+
+    private void setRobotPos(){
+        currentPos = new Vector2(transform.position.x, transform.position.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+        Debug.Log("Player detected");
+    }
+}
