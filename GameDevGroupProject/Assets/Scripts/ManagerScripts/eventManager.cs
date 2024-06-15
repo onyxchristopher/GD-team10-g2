8aa17acb0b735269e6363d2af4722355a1cf1197
LinkedBlades <pablo.99.garcia.sachetti@gmail.com>
2024-06-14 21:01:49 -0300

    Added Game Manager
    Game Manager GameObject has 3 scripts attached:
    sceneManager.cs : Will be responsible for loading every new scene after levels are done or relading the scene when player dies
    soundManager.cs: Will deal with every sound or music we decide to use
    eventManager: probably should be renamed to stateManager, keeps track of current game state and currently can Pause and Unpause the game when pressing "Enter". If we want to add animatios to the UI that keep working even when game is pause we should use Time.unscaledTime and Time.realtimeSinceStartup

---
+++ GameDevGroupProject/Assets/Scripts/ManagerScripts/eventManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eventManager : MonoBehaviour
+{
+
+    //Possible game states
+    public enum gameState { running, pause, loading };
+    private gameState currentState;
+
+    //Some stats we may want to keep track of 
+
+    //Variable to keep track of enemies killed
+    public int enemyKillCount;
+
+    //Variable to keep track of player deaths
+    public int playerDeahts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = gameState.running;
+
+        enemyKillCount = 0;
+        playerDeahts = 0;
+
+        if (currentState == gameState.running)
+        {
+            Debug.Log("Gamestate is running");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Return current game state
+    public gameState CurrentGameState()
+    {
+        return currentState;
+    }
+
+    //Changes game state to pause
+    public void PauseMenu()
+    {
+        //Pause Game
+        if (currentState == gameState.running)
+        {
+            currentState = gameState.pause;
+            Time.timeScale = 0;
+            Debug.Log("Game as been paused");
+        }
+
+        //Unpause game
+        else if (currentState == gameState.pause)
+        {
+            currentState = gameState.running;
+            Time.timeScale = 1;
+            Debug.Log("Game as been unpaused");
+        }
+
+        return;
+    }
+
+}
---
+++ GameDevGroupProject/Assets/Scripts/ManagerScripts/sceneManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//We will use this script to everything related to Loading, Unloading scenes and information about the level.
+public class sceneManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
