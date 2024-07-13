24856484695b2b5e1c5b3a5115113eaa83d22f23
Onyx Christopher <onyx.t.christopher@gmail.com>
2024-07-13 00:24:08 -0400

    Level 1 art created and integrated

--- GameDevGroupProject/Assets/Scripts/bullet.cs
+++ GameDevGroupProject/Assets/Scripts/bullet.cs
@@ -11,7 +11,7 @@ public class bullet : MonoBehaviour
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
-        rb.velocity = -transform.right * speed;
+        rb.velocity = transform.right * speed;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
