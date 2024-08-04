70b3e527726cd702a4f5adad3caed9b990ccbd4b
Onyx Christopher <onyx.t.christopher@gmail.com>
2024-08-04 01:52:09 -0400

    Roof complete

--- GameDevGroupProject/Assets/Scripts/PowerUpScripts/pulseCollision.cs
+++ GameDevGroupProject/Assets/Scripts/PowerUpScripts/pulseCollision.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class pulseCollision : MonoBehaviour
 {
 
-    soundManager sManager;
+    private soundManager sndManager;
 
     void Awake()
     {
-        sManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<soundManager>();
+        sndManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<soundManager>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Enemy")
         {
-            sManager.PlaySFX(sManager.enemyDefeated);
+            sndManager.PlaySFX(sndManager.enemyDefeated);
             Destroy(other.gameObject.transform.parent.gameObject);
         }
     }
