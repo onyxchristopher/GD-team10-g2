bf3ad2148993b10aa8dee01112c8cbb9fc4dc9c7
anjelomati <anjeloomati@gmail.com>
2024-08-30 23:38:53 +0800

    added NewLevelState() hook to play button's onclick

--- GameDevGroupProject/Assets/Scripts/ManagerScripts/gameController.cs
+++ GameDevGroupProject/Assets/Scripts/ManagerScripts/gameController.cs
@@ -6,7 +6,13 @@ using UnityEngine.UI;
 public class gameController : MonoBehaviour
 {
     // Possible game states
-    public enum gameState { running, pause, loading };
+    public enum gameState
+    {
+        running,
+        pause,
+        loading
+    };
+
     private gameState currentState;
 
 
@@ -20,6 +26,7 @@ public class gameController : MonoBehaviour
 
     // Stars obtained per level
     private int[] starsObtained;
+
     // Highest level reached by the player so far
     public int highestLevelReached = 1;
 
@@ -28,7 +35,7 @@ public class gameController : MonoBehaviour
     private soundManager sndManager;
     private playerBehavior pBehavior;
     private cameraMovement camMove;
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -52,7 +59,6 @@ public class gameController : MonoBehaviour
     //Pauses and unpauses game state
     public void Pause()
     {
-        
         sndManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<soundManager>();
         sndManager.PauseBGM();
 
@@ -85,6 +91,11 @@ public class gameController : MonoBehaviour
     // Reset the player to a fresh start on a level
     public void NewLevelState()
     {
+        // these are null for some reason so we need to get them right in this function
+        pBehavior = GameObject.FindGameObjectWithTag("Player").GetComponent<playerBehavior>();
+        scnManager = GameObject.FindGameObjectWithTag("SceneManager").GetComponent<sceneManager>();
+        camMove = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<cameraMovement>();
+        
         //TP player to current level start
         pBehavior.MovePlayer(new Vector3(6, 150 * (scnManager.currLevel - 1) + 2, 0));
         //Remove powerups
@@ -99,7 +110,8 @@ public class gameController : MonoBehaviour
     */
     public void SetStars(int level, int stars)
     {
-        if (starsObtained[level - 1] < stars){
+        if (starsObtained[level - 1] < stars)
+        {
             starsObtained[level - 1] = stars;
         }
     }
@@ -111,13 +123,16 @@ public class gameController : MonoBehaviour
         Instantiate(achievScreen);
 
         // Iterate through all levels
-        for (int i = 1; i <= 5; i++){
+        for (int i = 1; i <= 5; i++)
+        {
             // Iterate through all stars in current level
-            for (int j = 1; j <= 3; j++){
-                if (starsObtained[i - 1] >= j){
+            for (int j = 1; j <= 3; j++)
+            {
+                if (starsObtained[i - 1] >= j)
+                {
                     GameObject.Find($"L{i}S{j}")
-                    .GetComponent<Image>()
-                    .color = new Color32(255, 255, 255, 255);
+                        .GetComponent<Image>()
+                        .color = new Color32(255, 255, 255, 255);
                 }
             }
         }
@@ -148,5 +163,4 @@ public class gameController : MonoBehaviour
 
         Debug.Log("End of coroutine: " + Time.time);
     }
-
 }
